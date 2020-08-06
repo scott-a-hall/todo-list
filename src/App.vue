50dd1363f4caa26c39e7be21e82ec776424aaf03
scott-a-hall <hallsa3@yahoo.com>
2020-08-06 17:19:10 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <md-card-header>
+      <div class="title">ToDo List</div>
+    </md-card-header>
+    <md-card>
+      <md-field>
+        <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder=" Add a todo" />
+      </md-field>
+      <ul class="todos">
+        <li v-for="todo in todos" :key="todo.id">
+          <md-input type="text" v-model="todo.label" placeholder="update todo">
+            <span
+              v-if="todo.editing === false"
+              class="editing"
+              :class="{editing: todo == editedTodo}"
+              @dblclick="editTodo(todo)"
+            >{{ todo.label }}</span>
+          </md-input>
+          <input type="checkbox" @change="completeTodo(todo)" :checked="todo.isComplete" />
+          <md-button class="md-raised" @click="removeTodo(todo)">Delete</md-button>
+        </li>
+      </ul>
+    </md-card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+      completed: "",
+      editedTodo: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        editing: false,
+      });
+      this.currentTodo = "";
+    },
+    completeTodo(todo) {
+      todo.isComplete = !todo.isComplete;
+    },
+    editTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos[index].editing = !this.todos[index].editing;
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.title {
+  font-size: 48px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: #006a6a;
+}
+.md-card {
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+}
+ul {
+  list-style: none;
+}
+.md-button {
+  color: red;
+}
+li {
+  display: flex;
+  padding-top: 5px;
+  padding-bottom: 5px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdField, MdCard, MdButton, MdCheckbox } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+
+Vue.use(MdField);
+Vue.use(MdCard);
+Vue.use(MdButton);
+Vue.use(MdCheckbox);
 
 Vue.config.productionTip = false
 
